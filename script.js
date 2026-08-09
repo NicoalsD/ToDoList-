673d70095ab0f7aa8ece2e89261b0d2e3f19ece7
Nicolas Diaz <22334455nico@gmail.com>
2026-08-09 06:00:19 -0500

    Fix: Refactor task addition logic and improve element structure

--- script.js
+++ script.js
@@ -1,118 +1,57 @@
-// // Como seleccionar elementos
-
-// let parrafo = document.getElementById('parrafo') //obtiene el elemento por el ID
-// console.log(parrafo)
-
-// let myDiv = document.querySelector(".my-div") //obtiene el primer elemento que coincida con el selector CSS
-// console.log(myDiv)
-
-// let list = document.querySelectorAll("ul > li") //lista de nodos que hayamos seleccionado
-// console.log(list)
-
-// // Modificar elementos 
-
-// parrafo.innerText = "Manipulando el DOM" // modificar texto
-
-// parrafo.innerHTML = "<h2>hola</h2>"
-// parrafo.setAttribute('class', 'gey') //podemos cambiar atributos de un elemento
-
-// myDiv.style.backgroundColor = "#000"; //para modificar elementos css
-
-// let oldDiv = document.querySelector(".old-div") ;
-// document.body.removeChild(oldDiv)
-
-// let newDiv = document.createElement("div") // se crea el elemento en memoria
-// newDiv.innerText = "alo" // se modifica el elemento en memoria
-// document.body.appendChild(newDiv) // se mueve el elemento al body
-
-// let button = document.querySelector("button")
-
-// button.addEventListener("click", function() {
-//     alert("boton presionado");
-// }) // Un listener de eventos, escoges que evento y que va hacer dentro de la función anonima 
-
-// let button2 = document.getElementById("boton2")
-
-// function showMessage(message) {
-//     alert(message)
-// }
-
-// button2.addEventListener("click", showMessage.bind(null, "Hola"))
-
-// let inputTask = document.querySelector("#inputTask")
-
-// let addTaskButton = document.querySelector("#addTask")
-
-
-// let taskList = document.querySelector("#taskList")
-
-// addTaskButton.addEventListener("click", function(){
-//     if (inputTask.value.trim() != "") {
-//         newTask = document.createElement("li")
-//         newTask.innerText = inputTask.value 
-//         taskList.appendChild(newTask)
-//         inputTask.value = ""
-//     }
-// })
-
-let inputTask = document.querySelector("#inputTask")
-
+// Add task form elements
+let addTaskForm = document.querySelector("#add-task-form")
+let inputTask = document.querySelector("#input-task")
 let addTaskButton = document.querySelector("#addTaskButton")
 
-let form = document.querySelector("#form-add-task")
-
-// let task = document.querySelector("#task")
-
-let taskContainer = document.querySelector("#task-list-container")
+// Task container 
+let taskListContainer = document.querySelector("#task-list-container")
 
 
 
-form.addEventListener("submit", function(e) {
+addTaskForm.addEventListener("submit", function(e) {
     e.preventDefault()
     console.log(e)
     if (inputTask.value.trim() != "") {
+        // Task elements
+        let task = document.createElement("li")
+        task.classList.add("task")
 
-        let newTaskTittle = document.createElement("h2")
-        let newTask = document.createElement("div")
-        newTask.classList.add("task")
+        let label = document.createElement("label")
+
+        let checkTask = document.createElement("input")
+        checkTask.classList.add("check-task")
+        checkTask.setAttribute("type", "checkbox")
         
-        let newTaskCheckbox = document.createElement("input")
-        newTaskCheckbox.setAttribute("type", "checkbox")
-        newTaskCheckbox.setAttribute("class", "checkTaskButton")
-        newTask.appendChild(newTaskCheckbox)
-
-        newTaskTittle.textContent = inputTask.value
-        // newTaskTittle.setAttribute("class", "taskTittle")
-
-        newTaskTittle.classList.add("taskTittle")
-
-        newTask.appendChild(newTaskTittle)
-
-
-        let newTaskDelete = document.createElement("button")
-        // newTaskDelete.setAttribute("type", "button")
-        newTaskDelete.innerText = "Delete"
-        newTaskDelete.setAttribute("class", "deleteTaskButton")
-        newTaskDelete.setAttribute("value", "Delete")
-
-        newTask.appendChild(newTaskDelete)
-
-
-        taskContainer.appendChild(newTask)
+        let taskTitle = document.createElement("span")
+        taskTitle.classList.add("task-title")
+        taskTitle.textContent = inputTask.value
+    
+        
+        let deleteTaskButton = document.createElement("button")
+        deleteTaskButton.classList.add("delete-task-button")
+        deleteTaskButton.textContent = "Delete task"
+        
+        label.appendChild(checkTask)
+        label.appendChild(taskTitle)
+        task.appendChild(label)
+        task.appendChild(deleteTaskButton)
+        taskListContainer.appendChild(task)
 
         inputTask.value = ""
     }
 })
 
 
-// Eliminar task y marcar completada
-taskContainer.addEventListener("click", function(e) {
-    console.log(e.target.classList)
-    if (e.target.classList.contains("deleteTaskButton") ) {
+// Check Task
+taskListContainer.addEventListener("click", function(e){
+    if (e.target.classList.contains("check-task")) {
+        e.target.parentElement.querySelector(".task-title").classList.toggle("active")
+    }
+})
+
+// Delete task
+taskListContainer.addEventListener("click", function(e) {
+    if (e.target.classList.contains("delete-task-button") ) {
         e.target.parentElement.remove()
     } 
-    
-    else if (e.target.classList.contains("checkTaskButton")) {
-        e.target.parentElement.querySelector(".taskTittle").classList.toggle("active")
-    }
 })
